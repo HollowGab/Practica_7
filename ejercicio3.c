9b7eb976d5467c4a6ebdd0eb3cde77720e76880f
HollowGab <gabri.areani411@gmail.com>
2025-03-27 14:41:29 -0600

    Update ejercicio3.c

--- ejercicio3.c
+++ ejercicio3.c
@@ -1,6 +1,7 @@
 //Arellanes Aniceto Gabriel
 // ejercicio1
 #include<stdio.h>
+#include<math.h>
 void main ()
 {
   int a, b;
@@ -8,42 +9,75 @@ void main ()
   int res;
   printf("Menu\n  1. Suma\n 2. Resta\n 3. Multiplicacion\n 4. Division\n 5. Raiz Cuadrada\n 6. Potencia\n 0. Salir\n");
   printf("Selecciona la operacion que quieres realizar: ");
-  scanf (%i,&op);
+  scanf ("%i",&op);
   while (op>0 || op<6)
     {
   switch(op)
   {
-    case 1;
+    case 1:
     printf("SUMA \n");
     printf("Dame un numero:");
     scanf("%i",&a);
     printf("Dame otro numero:");
     scanf("%i",&b);
-    res = a+b;
-    printf("El resultado es: %d",
+    res= a+b;
+    printf("El resultado es: %d",res);
+    break;
+    
+    case 2:
+    printf("RESTA \n");
+    printf("Dame un numero:");
+    scanf("%i",&a);
+    printf("Dame otro numero:");
+    scanf("%i",&b);
+    res= a-b;
+    printf("El resultado es: %d",res);
+    break;
+    
+    case 3:
+    printf("MULTIPLICACION \n");
+    printf("Dame un numero:");
+    scanf("%i",&a);
+    printf("Dame otro numero:");
+    scanf("%i",&b);
+    res= a*b;
+    printf("El resultado es: %d",res);
     
     break;
-    case 2;
-     printf("RESTA \n");
-    break;
-    case 3;
-     printf("MULTIPLICACION \n");
     
+    case 4:
+    printf("DIVISION \n");
+    printf("Dame un numero:");
+    scanf("%i",&a);
+    printf("Dame otro numero:");
+    scanf("%i",&b);
+    res= a/b;
+    printf("El resultado es: %d",res);
     break;
-    case 4;
-     printf("DIVISION \n");
+    
+    case 5:
+    printf("RAIZ CUADRADA \n");
+    printf("Dame el numero del que quieres su raiz:");
+    scanf("%i",&a);
+    res= sqrt(a);
+    printf("El resultado es: %d",res);
     break;
-    case 5;
-     printf("RAIZ CUADRADA \n");
+    
+    case 6:
+    printf("POTENCIA \n");
+    printf("Dame la base:");
+    scanf("%i",&a);
+    printf("Dame el exponente:");
+    scanf("%i",&b);
+    res= pow(a,b);
+    printf("El resultado es: %d",res);
     break;
-    case 6;
-     printf("POTENCIA \n");
-    break;
+    
     default:
      printf("Opcion no valida\n ");
     printf("Menu\n  1. Suma\n 2. Resta\n 3. Multiplicacion\n 4. Division\n 5. Raiz Cuadrada\n 6. Potencia\n 0. Salir\n");
     printf("Selecciona la operacion que quieres realizar: ");
-    scanf (%i,&op);
+    scanf ("%i",&op);
   }
  printf("\t Fin del programa.\n");
     }
