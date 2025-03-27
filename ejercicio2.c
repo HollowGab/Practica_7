5e569ba5719b05863d428ab70cb91b35822de021
HollowGab <gabri.areani411@gmail.com>
2025-03-27 13:41:54 -0600

    Update ejercicio2.c

--- ejercicio2.c
+++ ejercicio2.c
@@ -1,1 +1,19 @@
-
+//Arellanes Aniceto Gabriel
+// ejercicio2
+#include<stdio.h>
+int main ()
+{
+  int a, b;
+  a = 3;
+  b = 2;
+  if(a > b)
+  {
+    printf("\ta (%d) es mayor a b (%d).\n",a,b);
+  }
+  else
+  {
+    
+  }
+  printf("\t El programa sigue su flujo.\n");
+  return 0;
+}
