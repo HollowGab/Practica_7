7858f6f4c7bca0cbe354d6c9864617559676108f
HollowGab <gabri.areani411@gmail.com>
2025-03-27 13:45:08 -0600

    Update ejercicio3.c

--- ejercicio3.c
+++ ejercicio3.c
@@ -1,1 +1,25 @@
+//Arellanes Aniceto Gabriel
+// ejercicio1
+#include<stdio.h>
+int main ()
+{
+  int a, b;
+  a = 3;
+  b = 2;
+  switch()
+  {
+    case 1;
+    
+    break;
+    case 2;
 
+    break;
+    case 3;
+
+    break;
+    
+    default:
+  }
+
+  return 0;
+}
